165036af733c80899e3b951dd2f0023d802392fb
adonais <lihualove1@163.com>
2025-08-08 18:34:51 +0800

    Small change

--- xpcom/ds/nsDeque.cpp
+++ xpcom/ds/nsDeque.cpp
@@ -59,18 +59,7 @@ size_t nsDequeBase::SizeOfExcludingThis(
 void nsDequeBase::Empty() {
   if (mSize && mData) {
 #ifdef TT_MEMUTIL
-    static const uint32_t dwNonTemporalDataSizeMin = GetNonTemporalDataSizeMin_tt();
-    const uint32_t dwDataSize = mCapacity * sizeof(*mData);
-
-    if (dwDataSize < dwNonTemporalDataSizeMin ||
-        NON_TEMPORAL_STORES_NOT_SUPPORTED == dwNonTemporalDataSizeMin)
-    {
-    memset(mData, 0, mCapacity * sizeof(*mData));
-  }
-    else
-    {
-        memset_nontemporal_tt(mData, 0, mCapacity * sizeof(*mData));
-    }
+    memset_nontemporal_tt(mData, 0, mCapacity * sizeof(*mData));
 #else
     memset(mData, 0, mCapacity * sizeof(*mData));
 #endif
