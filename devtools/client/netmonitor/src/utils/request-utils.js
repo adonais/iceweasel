b91e2092e44241ba7aaab94e4e91844afdee5426
Hubert Boma Manilla <hmanilla@mozilla.com>
2026-02-06 15:25:15 +0000

    Bug 2010743 - [devtools] Avoid prototype pollution when parsing form data  a=dmeehan
    Highlights of this patch
    
    - Use `Object.create(null)` instead of `{}` when mapping the format data sections array to a array of dictionaries.
    - Use `URLSearchParams` to parse the format-data sections
    - Added tests to assert the fix for the prototype pollution.
    
    Original Revision: https://phabricator.services.mozilla.com/D279525
    
    Differential Revision: https://phabricator.services.mozilla.com/D281774

--- devtools/client/netmonitor/src/utils/request-utils.js
+++ devtools/client/netmonitor/src/utils/request-utils.js
@@ -63,7 +63,6 @@ async function getFormDataSections(
       }
     }
   }
-
   return formDataSections;
 }
 
@@ -351,24 +350,29 @@ function parseQueryString(query) {
 /**
  * Parse a string of formdata sections into its components
  *
- * @param {string} sections - sections of formdata joined by &
- * @return {array} array of formdata params { name, value }
+ * @param {Array<string>} sections Array of sections of formdata
+ *                                 e.g ["", "a=x&b=y", "c=z"]
+ * @return {Array<object>}  Array of formdata params
+ *                          e.g [{ name: 'a', value: 'x' }, { name: 'b', value: 'y'}, { name: 'c', value: 'z'}]
  */
 function parseFormData(sections) {
-  if (!sections) {
+  if (!sections || !sections.length) {
     return [];
   }
+  const formDataParams = [];
+  const searchStr = sections
+    // Filter out empty sections
+    .filter(str => /\S/.test(str))
+    .join("&");
 
-  return sections
-    .replace(/^&/, "")
-    .split("&")
-    .map(e => {
-      const param = e.split("=");
-      return {
-        name: param[0] ? getUnicodeUrlPath(param[0]) : "",
-        value: param[1] ? getUnicodeUrlPath(param[1]) : "",
-      };
+  const params = new URLSearchParams(searchStr);
+  for (const [key, value] of params) {
+    formDataParams.push({
+      name: getUnicodeUrlPath(key),
+      value: getUnicodeUrlPath(value),
     });
+  }
+  return formDataParams;
 }
 
 /**
