8f8380fff379f41eb9417a1dde1ac5109a46ca54
Mark Striemer <mstriemer@mozilla.com>
2026-03-04 16:27:52 +0000

    Revert "Bug 2016733 - Ensure items in Applications handlers view are populated before test runs "  a=pascalc
    This reverts commit 334c16b089994297fbae78c2ba1edfee6f8bb2b9.
    
    Differential Revision: https://phabricator.services.mozilla.com/D285876

--- browser/components/preferences/tests/browser_filetype_dialog.js
+++ browser/components/preferences/tests/browser_filetype_dialog.js
@@ -15,11 +15,6 @@ let gDummyHandlers = [];
 let gOriginalPreferredMailHandler;
 let gOriginalPreferredPDFHandler;
 
-/**
- * @type {Promise<void>}
- */
-let appHandlerInitialized;
-
 registerCleanupFunction(function () {
   function removeDummyHandlers(handlers) {
     // Remove any of the dummy handlers we created.
@@ -132,10 +127,7 @@ add_setup(async function () {
   pdfHandlerInfo.preferredAction = Ci.nsIHandlerInfo.useHelperApp;
   gHandlerService.store(pdfHandlerInfo);
 
-  appHandlerInitialized = TestUtils.topicObserved("app-handler-loaded");
-
   await openPreferencesViaOpenPreferencesAPI("general", { leaveOpen: true });
-
   info("Preferences page opened on the general pane.");
 
   await gBrowser.selectedBrowser.contentWindow.promiseLoadHandlersList;
@@ -146,7 +138,6 @@ add_task(async function dialogShowsCorrectContent() {
   let win = gBrowser.selectedBrowser.contentWindow;
 
   let container = win.document.getElementById("handlersView");
-  await appHandlerInitialized;
 
   // First, find the PDF item.
   let pdfItem = container.querySelector("moz-box-item[type='application/pdf']");
