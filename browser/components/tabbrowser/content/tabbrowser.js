1f917839dba532d490f0e1792524e95a5efe4e76
Kelly Cochrane <kcochrane@mozilla.com>
2026-03-05 16:52:41 +0000

    Bug 2015252 - Add 'Reverse Tabs' option to the tab context menu for split view tabs  a=pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D285706

--- browser/components/tabbrowser/content/tabbrowser.js
+++ browser/components/tabbrowser/content/tabbrowser.js
@@ -10311,9 +10311,14 @@ var TabContextMenu = {
     let contextSeparateSplitView = document.getElementById(
       "context_separateSplitView"
     );
+    let contextReverseSplitView = document.getElementById(
+      "context_reverseSplitView"
+    );
     let hasSplitViewTab = this.contextTabs.some(tab => tab.splitview);
     contextMoveTabToNewSplitView.hidden = !splitViewEnabled || hasSplitViewTab;
     contextSeparateSplitView.hidden = !splitViewEnabled || !hasSplitViewTab;
+    contextReverseSplitView.hidden =
+      !splitViewEnabled || !hasSplitViewTab || this.multiselected;
     if (splitViewEnabled) {
       contextMoveTabToNewSplitView.removeAttribute("data-l10n-id");
       contextMoveTabToNewSplitView.setAttribute(
@@ -10874,6 +10879,10 @@ var TabContextMenu = {
     splitviews.forEach(splitview =>
       gBrowser.unsplitTabs(splitview, "menu_separate")
     );
+  },
+
+  reverseSplitView() {
+    this.contextTab.splitview?.reverseTabs("menu");
   },
 
   addNewBadge(menuItem) {
