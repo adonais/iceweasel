f96aab33b86b9e5f70cd36214bfcf2fa1a534c0e
Ben Visness <bvisness@mozilla.com>
2026-03-13 07:26:26 +0000

    Bug 2021627: More precise comparison on MWasmLoadField.  a=pascalc DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D286802
    
    Differential Revision: https://phabricator.services.mozilla.com/D287351

--- js/src/jit/MIR-wasm.h
+++ js/src/jit/MIR-wasm.h
@@ -2535,7 +2535,7 @@ class MWasmLoadField : public MBinaryInstruction, public NoTypePolicy::Data {
   MWideningOp wideningOp_;
   AliasSet aliases_;
   wasm::MaybeTrapSiteDesc maybeTrap_;
-  mozilla::Maybe<wasm::RefTypeHierarchy> hierarchy_;
+  wasm::MaybeRefType maybeRefType_;
 
   MWasmLoadField(MDefinition* base, MDefinition* keepAlive, size_t offset,
                  mozilla::Maybe<uint32_t> structFieldIndex, MIRType type,
@@ -2548,7 +2548,7 @@ class MWasmLoadField : public MBinaryInstruction, public NoTypePolicy::Data {
         wideningOp_(wideningOp),
         aliases_(aliases),
         maybeTrap_(std::move(maybeTrap)),
-        hierarchy_(maybeRefType.hierarchy()) {
+        maybeRefType_(maybeRefType) {
     MOZ_ASSERT(offset <= INT32_MAX);
     // "if you want to widen the value when it is loaded, the destination type
     // must be Int32".
@@ -2598,9 +2598,7 @@ class MWasmLoadField : public MBinaryInstruction, public NoTypePolicy::Data {
   MWideningOp wideningOp() const { return wideningOp_; }
   AliasSet getAliasSet() const override { return aliases_; }
   wasm::MaybeTrapSiteDesc maybeTrap() const { return maybeTrap_; }
-  mozilla::Maybe<wasm::RefTypeHierarchy> hierarchy() const {
-    return hierarchy_;
-  }
+  wasm::MaybeRefType maybeRefType() const { return maybeRefType_; }
 
   bool congruentTo(const MDefinition* ins) const override {
     if (!ins->isWasmLoadField()) {
@@ -2611,7 +2609,7 @@ class MWasmLoadField : public MBinaryInstruction, public NoTypePolicy::Data {
            structFieldIndex() == other->structFieldIndex() &&
            wideningOp() == other->wideningOp() &&
            getAliasSet().flags() == other->getAliasSet().flags() &&
-           hierarchy() == other->hierarchy();
+           maybeRefType() == other->maybeRefType();
   }
 
   virtual AliasType mightAlias(const MDefinition* ins) const override;
