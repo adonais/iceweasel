eb87d1158b0e4808b9f0e5ff8f048db4a8efd429
Emilio Cobos Álvarez <emilio@crisal.io>
2026-03-17 11:54:19 +0000

    Bug 2011820 - appearance: {base,base-select} support via an @appearance-base rule. r=tlouw
    Differential Revision: https://phabricator.services.mozilla.com/D287818

--- layout/generic/nsContainerFrame.cpp
+++ layout/generic/nsContainerFrame.cpp
@@ -2285,7 +2285,7 @@ nsRect nsContainerFrame::ComputeSimpleTightBounds(
     DrawTarget* aDrawTarget) const {
   if (StyleOutline()->ShouldPaintOutline() || StyleBorder()->HasBorder() ||
       !StyleBackground()->IsTransparent(this) ||
-      StyleDisplay()->HasAppearance()) {
+      StyleDisplay()->HasNativeAppearance()) {
     // Not necessarily tight, due to clipping, negative
     // outline-offset, and lots of other issues, but that's OK
     return InkOverflowRect();
