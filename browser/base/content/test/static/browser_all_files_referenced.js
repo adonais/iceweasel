3e3a288dd61387d1d78f4603819a00fcde90ead6
Nathan Barrett <nbarrett@mozilla.com>
2026-03-18 14:55:31 +0000

    Bug 2022304 - create newtab specific bundles and remove dependency on toolkit vendor files r=thecount,home-newtab-reviewers,frontend-codestyle-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D287036

--- browser/base/content/test/static/browser_all_files_referenced.js
+++ browser/base/content/test/static/browser_all_files_referenced.js
@@ -191,6 +191,11 @@ var allowlist = [
   // extensions/pref/autoconfig/src/nsReadConfig.cpp
   { file: "resource://gre/defaults/autoconfig/prefcalls.js" },
 
+  // browser/extensions/newtab/actors/AboutNewTabChild.sys.mjs constructs the
+  // URL dynamically: `chrome://global/content/vendor/react${debugString}.js`
+  { file: "chrome://global/content/vendor/react-dev.js" },
+  { file: "chrome://global/content/vendor/react-dom-dev.js" },
+
   // browser/components/preferences/moreFromMozilla.js
   // These files URLs are constructed programatically at run time.
   {
