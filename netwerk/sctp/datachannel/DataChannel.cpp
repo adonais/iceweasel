d14dbd3f66e52ec605927ab0150cefb72100199c
rperta <rperta@mozilla.com>
2026-03-18 10:31:18 +0200

    Revert "Bug 2023512 - Micro-optimizations in netwerk r=valentin,necko-reviewers,webrtc-reviewers,jesup,mjf" for causing ipv6 tests related failures
    This reverts commit 114b8c673b76d90f7624b528b6eeb6e62a1fa557.

--- netwerk/sctp/datachannel/DataChannel.cpp
+++ netwerk/sctp/datachannel/DataChannel.cpp
@@ -355,7 +355,7 @@ void DataChannelConnection::ProcessQueuedOpens() {
   // Technically in an unspecified state, although no reasonable impl will leave
   // anything in here.
   mPending.clear();
-  for (const auto& channel : temp) {
+  for (auto channel : temp) {
     DC_DEBUG(("%p: Processing queued open for %p (%u)", this, channel.get(),
               channel->mStream));
     OpenFinish(channel);  // may end up back in mPending
