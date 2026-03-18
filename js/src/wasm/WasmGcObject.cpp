10b496e770ed0bd896d01c4c25b9d6e99ceb9f7c
Norisz Fay <nfay@mozilla.com>
2026-03-18 04:09:27 +0200

    Revert "Bug 2022052 - wasm: fix the allocation of OOL data in wasm structs. r=jseward" for causing SM bustages
    This reverts commit eee5f8267b28397f541c1799b89767222fcd9b0d.

--- js/src/wasm/WasmGcObject.cpp
+++ js/src/wasm/WasmGcObject.cpp
@@ -484,16 +484,12 @@ void WasmStructObject::obj_trace(JSTracer* trc, JSObject* object) {
   }
   if (MOZ_UNLIKELY(structType.totalSizeOOL_ > 0)) {
     uint8_t** addressOfOOLPtr = structObj.addressOfOOLPointer();
-    // *addressOfOOLPtr may be null if the struct was only partially initialized
-    // due to OOM during createStructOOL.
-    if (MOZ_LIKELY(*addressOfOOLPtr)) {
-      TraceBufferEdge(trc, &structObj, addressOfOOLPtr,
-                      "WasmStructObject outline data");
-      uint8_t* oolBase = *addressOfOOLPtr;
-      for (uint32_t offset : structType.outlineTraceOffsets_) {
-        AnyRef* fieldPtr = reinterpret_cast<AnyRef*>(oolBase + offset);
-        TraceManuallyBarrieredEdge(trc, fieldPtr, "wasm-struct-field");
-      }
+    TraceBufferEdge(trc, &structObj, addressOfOOLPtr,
+                    "WasmStructObject outline data");
+    uint8_t* oolBase = *addressOfOOLPtr;
+    for (uint32_t offset : structType.outlineTraceOffsets_) {
+      AnyRef* fieldPtr = reinterpret_cast<AnyRef*>(oolBase + offset);
+      TraceManuallyBarrieredEdge(trc, fieldPtr, "wasm-struct-field");
     }
   }
 }
