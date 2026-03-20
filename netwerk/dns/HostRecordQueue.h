38ab85b9f635c6fe48e2a90ff44d055c7cbbde03
Silaghi <asilaghi@mozilla.com>
2026-03-20 12:02:34 +0200

    Revert "Bug 2024108 - Split nsHostResolver::mLock into mDBLock (RWLock) and mQueueLock r=valentin,necko-reviewers,perftest-reviewers,mozperftest-reviewers,sparky" for causing build bustages at dns/nsHostResolver.cpp
    This reverts commit b1cf0a3ab6aa5de2ce651b09b3c4aadd1d013aec.

--- netwerk/dns/HostRecordQueue.h
+++ netwerk/dns/HostRecordQueue.h
@@ -23,45 +23,43 @@ class HostRecordQueue final {
   uint32_t PendingCount() const { return mPendingCount; }
   uint32_t EvictionQSize() const { return mEvictionQSize; }
 
-  // The lock protecting all queue state. Callers must hold this before calling
-  // any public method. When both mDBLock (from nsHostResolver) and mLock are
-  // needed, acquire mDBLock first.
-  mutable Mutex mLock{"nsHostResolver.mQueueLock"};
-
   // Insert the record to mHighQ or mMediumQ or mLowQ based on the record's
   // priority.
-  void InsertRecord(nsHostRecord* aRec, nsIDNSService::DNSFlags aFlags)
-      MOZ_REQUIRES(mLock);
+  void InsertRecord(nsHostRecord* aRec, nsIDNSService::DNSFlags aFlags,
+                    const MutexAutoLock& aProofOfLock);
   // Insert the record to mEvictionQ. In theory, this function should be called
   // when the record is not in any queue.
   void AddToEvictionQ(
       nsHostRecord* aRec, uint32_t aMaxCacheEntries,
-      nsRefPtrHashtable<nsGenericHashKey<nsHostKey>, nsHostRecord>& aDB)
-      MOZ_REQUIRES(mLock);
+      nsRefPtrHashtable<nsGenericHashKey<nsHostKey>, nsHostRecord>& aDB,
+      const MutexAutoLock& aProofOfLock);
 
   // Move aRec to the tail of mEvictionQ (the most-recently-used end).
-  void MoveToEvictionQueueTail(nsHostRecord* aRec) MOZ_REQUIRES(mLock);
+  void MoveToEvictionQueueTail(nsHostRecord* aRec,
+                               const MutexAutoLock& aProofOfLock);
 
   // Called for removing the record from mEvictionQ. When this function is
   // called, the record should be either in mEvictionQ or not in any queue.
-  void MaybeRenewHostRecord(nsHostRecord* aRec) MOZ_REQUIRES(mLock);
+  void MaybeRenewHostRecord(nsHostRecord* aRec,
+                            const MutexAutoLock& aProofOfLock);
   // Called for clearing mEvictionQ.
   void FlushEvictionQ(
-      nsRefPtrHashtable<nsGenericHashKey<nsHostKey>, nsHostRecord>& aDB)
-      MOZ_REQUIRES(mLock);
+      nsRefPtrHashtable<nsGenericHashKey<nsHostKey>, nsHostRecord>& aDB,
+      const MutexAutoLock& aProofOfLock);
   // Remove the record from the queue that contains it.
-  void MaybeRemoveFromQ(nsHostRecord* aRec) MOZ_REQUIRES(mLock);
+  void MaybeRemoveFromQ(nsHostRecord* aRec, const MutexAutoLock& aProofOfLock);
   // When the record's priority changes, move the record between pending queues.
-  void MoveToAnotherPendingQ(nsHostRecord* aRec, nsIDNSService::DNSFlags aFlags)
-      MOZ_REQUIRES(mLock);
+  void MoveToAnotherPendingQ(nsHostRecord* aRec, nsIDNSService::DNSFlags aFlags,
+                             const MutexAutoLock& aProofOfLock);
   // Returning the first record from one of the pending queue. When |aHighQOnly|
   // is true, returning the record from mHighQ only. When false, return the
   // record from mMediumQ or mLowQ.
-  already_AddRefed<nsHostRecord> Dequeue(bool aHighQOnly) MOZ_REQUIRES(mLock);
+  already_AddRefed<nsHostRecord> Dequeue(bool aHighQOnly,
+                                         const MutexAutoLock& aProofOfLock);
   // Clear all queues and is called only during shutdown. |aCallback| is invoked
   // when a record is removed from a queue.
-  void ClearAll(const std::function<void(nsHostRecord*)>& aCallback)
-      MOZ_REQUIRES(mLock);
+  void ClearAll(const std::function<void(nsHostRecord*)>& aCallback,
+                const MutexAutoLock& aProofOfLock);
 
  private:
   Atomic<uint32_t> mPendingCount{0};
