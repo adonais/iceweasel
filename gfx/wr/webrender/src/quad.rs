a90d593e430113e4a5880f0ef355eee558772876
Nicolas Silva <nical@fastmail.com>
2026-04-24 17:18:04 +0000

    Bug 2027798 - Fix position of bottom left corner in the tiled rounded rect path (Beta version).  a=diannaS DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D294868

--- gfx/wr/webrender/src/quad.rs
+++ gfx/wr/webrender/src/quad.rs
@@ -1175,8 +1175,8 @@ fn prepare_tiles(
                 );
                 let c_bl = DeviceRect::from_origin_and_size(
                     DevicePoint::new(
-                        rect.min.x,
-                        rect.max.y - r_bl.height,
+                        clip_device_rect.min.x,
+                        clip_device_rect.max.y - r_bl.height,
                     ),
                     r_bl,
                 );
@@ -2327,7 +2327,9 @@ impl QuadTileClassifier {
         &mut self,
         mask_region: DeviceRect,
     ) {
-        self.mask_regions.push(mask_region);
+        if !mask_region.is_empty() {
+            self.mask_regions.push(mask_region);
+        }
     }
 
     // TODO(gw): Make use of this to skip tiles that are completely clipped out in a follow up!
