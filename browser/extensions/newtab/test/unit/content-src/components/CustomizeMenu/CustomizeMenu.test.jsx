45035b83dd73db4f2331a5da30bc5c4ddebc5beb
Reem H <42309026+reemhamz@users.noreply.github.com>
2026-04-29 17:22:34 +0000

    Bug 2030873 - Nova wallpapers reset functionality.  a=dmeehan
    Original Revision: https://phabricator.services.mozilla.com/D294346
    
    Differential Revision: https://phabricator.services.mozilla.com/D297190

--- browser/extensions/newtab/test/unit/content-src/components/CustomizeMenu/CustomizeMenu.test.jsx
+++ browser/extensions/newtab/test/unit/content-src/components/CustomizeMenu/CustomizeMenu.test.jsx
@@ -59,6 +59,7 @@ describe("<CustomizeMenu>", () => {
       },
       enabledWidgets: { timerEnabled: false, listsEnabled: false },
       wallpapersEnabled: false,
+      wallpapersUserEnabled: false,
       activeWallpaper: null,
       pocketRegion: "US",
       mayHaveTopicSections: false,
@@ -187,6 +188,7 @@ describe("<CustomizeMenu>", () => {
       mayHaveTimerWidget: true,
       mayHaveListsWidget: true,
       wallpapersEnabled: true,
+      wallpapersUserEnabled: true,
       enabledWidgets: { timerEnabled: true, listsEnabled: true },
     };
 
@@ -201,6 +203,7 @@ describe("<CustomizeMenu>", () => {
     assert.strictEqual(child.prop("mayHaveTimerWidget"), true);
     assert.strictEqual(child.prop("mayHaveListsWidget"), true);
     assert.strictEqual(child.prop("wallpapersEnabled"), true);
+    assert.strictEqual(child.prop("wallpapersUserEnabled"), true);
     assert.deepEqual(child.prop("enabledWidgets"), {
       timerEnabled: true,
       listsEnabled: true,
