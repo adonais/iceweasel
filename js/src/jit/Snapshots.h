d5c48ed1b2560305e68693cecb02f3c2bc39580c
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2026-05-09 22:06:51 +0000

    Bug 2036978 - Increase RecoverOffset size.  a=dmeehan
    Original Revision: https://phabricator.services.mozilla.com/D299169
    
    Differential Revision: https://phabricator.services.mozilla.com/D299524

--- js/src/jit/Snapshots.h
+++ js/src/jit/Snapshots.h
@@ -402,7 +402,7 @@ class RecoverWriter {
   uint32_t instructionsWritten_;
 
  public:
-  SnapshotOffset startRecover(uint32_t instructionCount);
+  RecoverOffset startRecover(uint32_t instructionCount);
 
   void writeInstruction(const MNode* rp);
 
