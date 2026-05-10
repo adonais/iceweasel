64686f8854a92cacdc719372b387fb36c1e896c2
ffxbld <ffxbld@mozilla.com>
2026-05-10 20:42:26 +0000

    No Bug, mozilla-beta repo-update remote-settings mobile-experiments ct-logs -  a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D299589

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1784232478000000);
+static const PRTime kCTExpirationTime = INT64_C(1784491554000000);
 
 namespace mozilla::ct {
 
