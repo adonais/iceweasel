aaadb7344fabd3e6ff1eb57a2c7512da099bce36
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2026-05-19 15:05:27 +0000

    Bug 2040496 - Stop scheduling android-only toolchains and tasks on esr115 r=taskgraph-reviewers,jcristau a=dmeehan DONTBUILD
    Android isn't built on esr115, so none of the android-only toolchains
    and helper artifacts are actually consumed. Stop scheduling the AVD
    repack toolchains, skip cargo-apk vendoring in wrench-deps (which
    removes the recurring need to pin android-ndk-rs crates against
    esr115's bundled rustc), drop the android wrench/emulator/hardware
    jobs from the webrender kind, and filter android-labeled tasks out of
    the rebuild-cached-tasks action.
    
    Differential Revision: https://phabricator.services.mozilla.com/D301219

--- taskcluster/scripts/misc/wrench-deps-vendoring.sh
+++ taskcluster/scripts/misc/wrench-deps-vendoring.sh
@@ -15,13 +15,6 @@ mkdir .cargo
 cargo vendor --sync ./Cargo.toml > .cargo/config
 mkdir wrench-deps
 mv vendor .cargo wrench-deps/
-mkdir wrench-deps/cargo-apk
-# Until there's a version of cargo-apk published on crates.io that has
-# https://github.com/rust-windowing/android-ndk-rs/pull/236, we need to use
-# an unpublished version. Additionally, until we update the NDK version used
-# in gecko we must use our own patched version. See bug 1615148.
-(cd $MOZ_FETCHES_DIR/android-ndk-rs/cargo-apk; cargo update -p home --precise 0.5.5 && cargo update -p unicode-ident --precise 1.0.22 && cargo update -p syn@2.0.117 --precise 2.0.114)
-cargo install --locked --path $MOZ_FETCHES_DIR/android-ndk-rs/cargo-apk --root wrench-deps/cargo-apk cargo-apk
 
 ci-scripts/install-meson.sh
 mv meson wrench-deps/meson
