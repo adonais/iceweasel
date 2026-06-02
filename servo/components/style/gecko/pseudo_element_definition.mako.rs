25d992027e758584f5d55c49cc1698f53405f3e7
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2026-06-02 18:28:22 -0400

    Revert "Bug 2036615 - Allow unknown webkit pseudos to parse state pseudo-classes. r=firefox-style-system-reviewers,dshin" for causing various regressions.
    This reverts commit 7edb8221b92dc23c3c0b86f7827bd671e3d0738c.

--- servo/components/style/gecko/pseudo_element_definition.mako.rs
+++ servo/components/style/gecko/pseudo_element_definition.mako.rs
@@ -63,13 +63,9 @@ impl PseudoElement {
     fn flags_for_index(i: usize) -> PseudoStyleTypeFlags {
         static FLAGS: [PseudoStyleTypeFlags; PSEUDO_COUNT + 1] = [
         % for pseudo in PSEUDOS:
-        PseudoStyleTypeFlags::from_bits_truncate(${' | '.join(f"PseudoStyleTypeFlags::{flag}.bits()" for flag in pseudo.flags())}), // ${pseudo.name}
+        PseudoStyleTypeFlags::from_bits_truncate(${' | '.join(f"PseudoStyleTypeFlags::{flag}.bits()" for flag in pseudo.flags())}),
         % endfor
-        // Unknown ::-webkit-* pseudos allow user-action state pseudo-classes.
-        PseudoStyleTypeFlags::from_bits_truncate(
-            PseudoStyleTypeFlags::IS_PSEUDO_ELEMENT.bits() |
-            PseudoStyleTypeFlags::SUPPORTS_USER_ACTION_STATE.bits()
-        ),
+        PseudoStyleTypeFlags::NONE, // :-webkit-*
         ];
         FLAGS[i]
     }
