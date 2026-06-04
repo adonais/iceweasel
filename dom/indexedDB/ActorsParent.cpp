4baa1e988fa8d27bf67c65d37f3d74b6674fd6bf
Jari Jalkanen <jjalkanen@mozilla.com>
2026-06-04 02:22:45 +0000

    Bug 2043213: Wait some more if deserialization takes longer.  a=RyanVM DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D303306
    
    Differential Revision: https://phabricator.services.mozilla.com/D304491

--- dom/indexedDB/ActorsParent.cpp
+++ dom/indexedDB/ActorsParent.cpp
@@ -6444,7 +6444,8 @@ class DeserializeIndexValueHelper final : public Runnable {
         mLocale(aLocale),
         mCloneReadInfo(aCloneReadInfo),
         mUpdateInfoArray(aUpdateInfoArray),
-        mStatus(NS_ERROR_FAILURE) {}
+        mStatus(NS_ERROR_FAILURE),
+        mDone{false} {}
 
   nsresult DispatchAndWait() {
     // FIXME(Bug 1637530) Re-enable optimization using a non-system-principaled
@@ -6477,7 +6478,10 @@ class DeserializeIndexValueHelper final : public Runnable {
     QM_TRY(MOZ_TO_RESULT(
         SchedulerGroup::Dispatch(TaskCategory::Other, self.forget())));
 
-    lock.Wait();
+    while (!mDone) {
+      lock.Wait();
+    }
+
     return mStatus;
   }
 
@@ -6540,10 +6544,11 @@ class DeserializeIndexValueHelper final : public Runnable {
     mStatus = aStatus;
 
     MonitorAutoLock lock(mMonitor);
+    mDone = true;
     lock.Notify();
   }
 
-  Monitor mMonitor MOZ_UNANNOTATED;
+  Monitor mMonitor;
 
   const int64_t mIndexID;
   const KeyPath& mKeyPath;
@@ -6552,6 +6557,7 @@ class DeserializeIndexValueHelper final : public Runnable {
   StructuredCloneReadInfoParent& mCloneReadInfo;
   nsTArray<IndexUpdateInfo>& mUpdateInfoArray;
   nsresult mStatus;
+  bool mDone MOZ_GUARDED_BY(mMonitor);
 };
 
 auto DeserializeIndexValueToUpdateInfos(
