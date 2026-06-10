e83305f19513d25a955ae426e2eba13b91d832ad
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2026-06-10 00:10:13 -0400

    Revert "Bug 2045307 - Use cell's zone in MacroAssembler::emitWeapMapBarrierFastPath. a=RyanVM" for bustage.
    This reverts commit 529fde2053dd9421017dbef62c6bb56caa9bab1d.

--- js/src/jit/MacroAssembler.cpp
+++ js/src/jit/MacroAssembler.cpp
@@ -8411,13 +8411,9 @@ void MacroAssembler::emitWeapMapBarrierFastPath(ValueOperand value,
   // If the gray bit is set, then we *do* need a barrier.
   branchTestPtr(Assembler::NonZero, markWord, mask, barrier);
 
-  // Otherwise, the tenured cell needs a barrier only if its zone is being
-  // marked by an incremental GC.
-  Register zone = temp2;
-  loadPtr(Address(chunk, gc::ChunkZoneOffset), zone);
-  branchTest32(Assembler::NonZero,
-               Address(zone, Zone::offsetOfNeedsMarkingBarrier()), Imm32(0x1),
-               barrier);
+  // Otherwise, we don't need a barrier unless we're in the middle of
+  // an incremental GC.
+  branchTestNeedsMarkingBarrierAnyZone(Assembler::NonZero, barrier, temp1);
   bind(&done);
 }
 
