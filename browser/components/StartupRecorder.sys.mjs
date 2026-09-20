64a4d7a48e1ddaea0a2452c61415f3ea8761151d
adonais <lihualove1@163.com>
2026-09-21 04:32:51 +0800

    Do not use browser.startup.preXulSkeletonUI

--- browser/components/StartupRecorder.sys.mjs
+++ browser/components/StartupRecorder.sys.mjs
@@ -7,14 +7,7 @@ Cm.QueryInterface(Ci.nsIServiceManager);
 
 import { AppConstants } from "resource://gre/modules/AppConstants.sys.mjs";
 
-let firstPaintNotification = "widget-first-paint";
-// widget-first-paint fires much later than expected on Linux.
-if (
-  AppConstants.platform == "linux" ||
-  Services.prefs.getBoolPref("browser.startup.preXulSkeletonUI", false)
-) {
-  firstPaintNotification = "xul-window-visible";
-}
+let firstPaintNotification = "xul-window-visible";
 
 let win, canvas;
 let paints = [];
